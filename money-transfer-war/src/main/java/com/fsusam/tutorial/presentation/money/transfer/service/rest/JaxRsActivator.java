79d361fd4d7dead7456cb991f4679a193415903d
Fethi Susam <fethi.susam@ericsson.com>
2018-12-24 16:26:09 +0000

    refactoring

--- money-transfer-war/src/main/java/com/fsusam/tutorial/presentation/money/transfer/service/rest/JaxRsActivator.java
+++ money-transfer-war/src/main/java/com/fsusam/tutorial/presentation/money/transfer/service/rest/JaxRsActivator.java
@@ -1,17 +1,3 @@
-/*
- * *------------------------------------------------------------------------------
- * ******************************************************************************
- *  COPYRIGHT Ericsson 2018
- *
- *  The copyright to the computer program(s) herein is the property of
- *  Ericsson Inc. The programs may be used and/or copied only with written
- *  permission from Ericsson Inc. or in accordance with the terms and
- *  conditions stipulated in the agreement/contract under which the
- *  program(s) have been supplied.
- * ******************************************************************************
- * ------------------------------------------------------------------------------
- */
-
 package com.fsusam.tutorial.presentation.money.transfer.service.rest;
 
 import java.util.HashSet;
@@ -20,11 +6,6 @@ import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/**
- * A class extending {@link Application} and annotated with @ApplicationPath is the Java EE 6 "no XML" approach to activating JAX-RS.
- * <p>
- * Resources are served relative to the servlet path specified in the {@link ApplicationPath} annotation.
- */
 @ApplicationPath("/")
 public class JaxRsActivator extends Application {
 
